c7c939774685968c7b996c6a64c9ffe33f5c5f25
AWhiteElephant <AWhiteElephant@123.com>
2021-08-23 12:03:22 +0800

    second commit

--- src/main/java/com/cbx/GitTest.java
+++ src/main/java/com/cbx/GitTest.java
@@ -1,7 +1,13 @@
 package com.cbx;
 
+/**
+ * @author cbx
+ */
+
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
